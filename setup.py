0df500f4e9ef2f9fa85b60c5ae085d840b76d081
yamasakih <yamasaki.phone@gmail.com>
2019-06-09 08:51:24 +0900

    Add long description

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from setuptools import setup
 from predictiveness_curve import __version__
 
 
-long_description=''
+long_description = """
+Predictiveness curve is a method to display two graphs simultaneously. In both
+figures, the x-axis is risk percentile, the y-axis of one figure is the value
+of risk, and the y-axis of the other figure is true positive fractions. This
+makes it possible to visualize whether the model of risk fits in the medical
+field and which value of risk should be used as the basis for the model.
+See Am. J. Epidemiol. 2008; 167:362–368 for details.
+"""
 
 setup(
     name='predictiveness-curve',
@@ -18,7 +25,5 @@ setup(
     description='Plot predictiveness curve',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    keywords='',
     classifiers=['License :: OSI Approved :: MIT License',]
 )
-
