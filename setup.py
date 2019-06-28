1da539ea9c5e4ea4d34fc99a94fc6bbd36c966f4
kubor <rkubo.4w@gmail.com>
2019-06-29 00:21:14 +0900

    :art: Use config file for setuptools

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-__version__ = '0.2.1'
-
-with open("README.md") as f:
-    long_description = f.read()
-
-setup(
-    name='predictiveness-curve',
-    version=__version__,
-    url='https://github.com/yamasakih/predictiveness-curve',
-    license='MIT',
-    py_modules=['predictiveness_curve'],
-    python_requires='>=3.6',
-    author='Hiroyuki Yamasaki',
-    author_email='yamasaki.phone@gmail.com',
-    install_requires=['matplotlib', 'numpy'],
-    description='Plot predictiveness curve',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    classifiers=['License :: OSI Approved :: MIT License',]
-)
+setup()
