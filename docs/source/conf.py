75ee9b4ab007767b9ca98a512f0c77af32203bca
yamasakih <yamasaki.phone@gmail.com>
2019-07-10 17:08:17 +0900

    Change document style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import predictiveness_curve
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -38,6 +39,8 @@ release = predictiveness_curve.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -73,7 +76,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
